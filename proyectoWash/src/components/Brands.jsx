e1a230282d70d256c80749f80d1f0ced9df4b8e2
Armasangel <angel.armasher@gmail.com>
2026-06-23 11:57:57 -0600

    Enhance Brands component with service details and brands
    Added information about services and brands offered, including a list of specific brands in pill format.

--- proyectoWash/src/components/Brands.jsx
+++ proyectoWash/src/components/Brands.jsx
@@ -2,7 +2,26 @@ import './Brands.css';
 function Brands() {
     return (
         <section className="brands">
-             <span>Angel</span>
+             <span> ━━Trabajamos todas las marcas</span>
+             <p>
+                <span> Servicio y refacciones para carros mezcladores y molinos principales </span>
+                <span>marcas del mercado</span>
+             </p>
+            <div class="pill-container">
+                <p>
+                    <div class="pill"><span class="rombo">◆</span>TRIOLET</div>
+                    <div class="pill"><span class="rombo">◆</span> ROTOMIX</div>
+                    <div class="pill"><span class="rombo">◆</span>KIRBYMEX</div>
+                    <div class="pill"><span class="rombo">◆</span>TORMEX</div>
+                    <div class="pill"><span class="rombo">◆</span>SUPER STAR</div>
+                    <div class="pill"><span class="rombo">◆</span>HAYBUSTER</div>
+                    <div class="pill"><span class="rombo">◆</span>FARMHAND</div>
+                </p>
+                <p>
+                    <div class="pill"><span class="rombo">◆</span>JONES MFG</div>
+                </p>
+             </div>
+
         </section>
     );
 }
