7c81dbf63545e48f577b1af7b216d99062b308d2
Armasangel <angel.armasher@gmail.com>
2026-06-23 12:58:48 -0600

    Actualización de las pildoras, el la estructura de la function y algunos estilos adicionales para centrar los elementos

--- proyectoWash/src/components/Brands.jsx
+++ proyectoWash/src/components/Brands.jsx
@@ -2,26 +2,21 @@ import './Brands.css';
 function Brands() {
     return (
         <section className="brands">
-             <span> ━━Trabajamos todas las marcas</span>
-             <p>
-                <span> Servicio y refacciones para carros mezcladores y molinos principales </span>
-                <span>marcas del mercado</span>
-             </p>
-            <div class="pill-container">
-                <p>
-                    <div class="pill"><span class="rombo">◆</span>TRIOLET</div>
-                    <div class="pill"><span class="rombo">◆</span> ROTOMIX</div>
-                    <div class="pill"><span class="rombo">◆</span>KIRBYMEX</div>
-                    <div class="pill"><span class="rombo">◆</span>TORMEX</div>
-                    <div class="pill"><span class="rombo">◆</span>SUPER STAR</div>
-                    <div class="pill"><span class="rombo">◆</span>HAYBUSTER</div>
-                    <div class="pill"><span class="rombo">◆</span>FARMHAND</div>
-                </p>
-                <p>
-                    <div class="pill"><span class="rombo">◆</span>JONES MFG</div>
-                </p>
-             </div>
+            <div class="header-info">
+                <h3>━━ Trabajamos todas las marcas</h3>
+                <p>Servicio de refacciones para carros mezcladores y molinos de las principales marcas del mercado</p>
+            </div> 
 
+            <div className="pill-container">
+                <div className="pill"><span className="rombo">◆</span> TRIOLIET</div>
+                <div className="pill"><span className="rombo">◆</span> ROTOMIX</div>
+                <div className="pill"><span className="rombo">◆</span> KIRBYMEX</div>
+                <div className="pill"><span className="rombo">◆</span> TORMEX</div>
+                <div className="pill"><span className="rombo">◆</span> SUPER STAR</div>
+                <div className="pill"><span className="rombo">◆</span> HAYBUSTER</div>
+                <div className="pill"><span className="rombo">◆</span> FARMHAND</div>
+                <div className="pill"><span className="rombo">◆</span> JONES MFG</div>
+            </div>
         </section>
     );
 }
